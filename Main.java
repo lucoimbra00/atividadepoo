af05802490a90a48d6e26799117ef5194be18915
lucoimbra00 <luiza.alfonso@edu.pucrs.br>
2026-04-01 20:49:19 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Aplicacao aplicacao = new Aplicacao();
-        aplicacao.inicializa();
-        aplicacao.executa();
-    }
-}
